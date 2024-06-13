1f01d4ef65096e08708740420706e4e2c9d577f5
Taokyla <10359761+Taokyla@users.noreply.github.com>
2024-06-13 14:35:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.taokyla.com": "https://registry-1.docker.io",
+  "quay.taokyla.com": "https://quay.io",
+  "gcr.taokyla.com": "https://gcr.io",
+  "k8s-gcr.taokyla.com": "https://k8s.gcr.io",
+  "k8s.taokyla.com": "https://registry.k8s.io",
+  "ghcr.taokyla.com": "https://ghcr.io",
+  "cloudsmith.taokyla.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
